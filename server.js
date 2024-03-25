017c205965c280246c0f41a6c48d6f73599e35b5
uberflip-kumaran <kumaran.perinpanatan@uberflip.com>
2024-03-25 07:15:00 -0400

    update keys configuration

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 const configuration = {
-  key: [
+  keys: [
     {
       alg: "RS256",
       e: "AQAB",
