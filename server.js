0707938c44d69e8d7139838deb24f572ff62c323
uberflip-kumaran <kumaran.perinpanatan@uberflip.com>
2024-03-24 21:49:16 -0400

    more code commented

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.get("/rest/getAllUsers", (req, res) => {
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // //Middlewares
-app.use(express.static(__dirname + "/public"));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// app.use(express.static(__dirname + "/public"));
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
 // const configuration = {
 //   key: [
 //     {
