3cbc2763697895bb98e0c0160b710b2a8aedd4be
uberflip-kumaran <kumaran.perinpanatan@uberflip.com>
2024-03-24 22:34:28 -0400

    uncomment oidc import

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import Provider from "oidc-provider";
+import Provider from "oidc-provider";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -96,89 +96,89 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-// const configuration = {
-//   key: [
-//     {
-//       alg: "RS256",
-//       e: "AQAB",
-//       key_ops: ["verify"],
-//       kty: "RSA",
-//       n: "0VJwEa4fiRDv4R6HMNEJZDR3_UAMGcrMhkR8aLhOLaOkV1sh84epY5xlpTmDiyCFf9bRDhSpnSB3QMOqzkBEZSlmd53P3hV-Wo-zRh48r1KzRdD6MDZU0oL-OyYgTP_VP1TZcT0oC5LG__p9Jex8SLpwzuHARpMuJyZ01wp5tUVkAbdGzf50-pQHoQNmXYoeojnG-alRMVH8gtuZ5oUn-h0GBSBspcAx5Jd_ifwX9uBkmYRjNFQQbslL69LZVcKvBwmPmeSxMI2TW0k8EJ-uR3Cw_5z1upG1vnKsKNpMuUsnwRduzncGaafz-MWDUIvVUYxMkRBjWRG0pPhDEb3E9w",
-//       use: "sig",
-//       kid: "c92834c9af14ade755dcefd90b5d7600",
-//     },
-//   ],
-//   clients: [
-//     {
-//       client_id: "oidcCLIENT",
-//       client_secret: "Some_super_secret",
-//       grant_types: ["authorization_code"],
-//       redirect_uris: [
-//         /* "http://localhost:8080/auth/login/callback",
-//         "https://oidcdebugger.com/debug", */
-//         "http://localhost:8055",
-//       ],
-//       features: {
-//         introspection: { enabled: true },
-//         revocation: { enabled: true },
-//       },
-//       formats: {
-//         AccessToken: "jwt",
-//       },
-//       claims: {
-//         openid: ["sub"],
-//         email: ["email", "email_verified"],
-//         profile: [
-//           "name",
-//           "family_name",
-//           "given_name",
-//           "profile",
-//           "picture",
-//           "updated_at",
-//         ],
-//       },
-//       async findAccount(ctx, id) {
-//         return {
-//           accountId: id,
-//           async claims(use, scope) {
-//             return {
-//               sub: id, // Usually a user id
-//               email: "user@example.com",
-//               email_verified: true,
-//               name: "John Doe",
-//               given_name: "John",
-//               family_name: "Doe",
-//               // Add more claims as per your application's requirements
-//             };
-//           },
-//         };
-//       },
-//       response_types: ["code"],
+const configuration = {
+  key: [
+    {
+      alg: "RS256",
+      e: "AQAB",
+      key_ops: ["verify"],
+      kty: "RSA",
+      n: "0VJwEa4fiRDv4R6HMNEJZDR3_UAMGcrMhkR8aLhOLaOkV1sh84epY5xlpTmDiyCFf9bRDhSpnSB3QMOqzkBEZSlmd53P3hV-Wo-zRh48r1KzRdD6MDZU0oL-OyYgTP_VP1TZcT0oC5LG__p9Jex8SLpwzuHARpMuJyZ01wp5tUVkAbdGzf50-pQHoQNmXYoeojnG-alRMVH8gtuZ5oUn-h0GBSBspcAx5Jd_ifwX9uBkmYRjNFQQbslL69LZVcKvBwmPmeSxMI2TW0k8EJ-uR3Cw_5z1upG1vnKsKNpMuUsnwRduzncGaafz-MWDUIvVUYxMkRBjWRG0pPhDEb3E9w",
+      use: "sig",
+      kid: "c92834c9af14ade755dcefd90b5d7600",
+    },
+  ],
+  clients: [
+    {
+      client_id: "oidcCLIENT",
+      client_secret: "Some_super_secret",
+      grant_types: ["authorization_code"],
+      redirect_uris: [
+        /* "http://localhost:8080/auth/login/callback",
+        "https://oidcdebugger.com/debug", */
+        "http://localhost:8055",
+      ],
+      features: {
+        introspection: { enabled: true },
+        revocation: { enabled: true },
+      },
+      formats: {
+        AccessToken: "jwt",
+      },
+      claims: {
+        openid: ["sub"],
+        email: ["email", "email_verified"],
+        profile: [
+          "name",
+          "family_name",
+          "given_name",
+          "profile",
+          "picture",
+          "updated_at",
+        ],
+      },
+      async findAccount(ctx, id) {
+        return {
+          accountId: id,
+          async claims(use, scope) {
+            return {
+              sub: id, // Usually a user id
+              email: "user@example.com",
+              email_verified: true,
+              name: "John Doe",
+              given_name: "John",
+              family_name: "Doe",
+              // Add more claims as per your application's requirements
+            };
+          },
+        };
+      },
+      response_types: ["code"],
 
-//       //other configurations if needed
-//     },
-//   ],
-//   async findAccount(ctx, id) {
-//     return {
-//       accountId: id,
-//       async claims(use, scope) {
-//         return {
-//           sub: id, // Usually a user id
-//           email: "user@example.com",
-//           email_verified: true,
-//           name: "John Doe",
-//           given_name: "John",
-//           family_name: "Doe",
-//           preferred_username: "johndoe",
-//           // Add more claims as per your application's requirements
-//         };
-//       },
-//     };
-//   },
-//   pkce: {
-//     required: () => false,
-//   },
-// };
+      //other configurations if needed
+    },
+  ],
+  async findAccount(ctx, id) {
+    return {
+      accountId: id,
+      async claims(use, scope) {
+        return {
+          sub: id, // Usually a user id
+          email: "user@example.com",
+          email_verified: true,
+          name: "John Doe",
+          given_name: "John",
+          family_name: "Doe",
+          preferred_username: "johndoe",
+          // Add more claims as per your application's requirements
+        };
+      },
+    };
+  },
+  pkce: {
+    required: () => false,
+  },
+};
 
 // const oidc = new Provider(`http://localhost:${PORT}`, configuration);
 
