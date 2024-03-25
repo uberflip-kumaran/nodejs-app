725b896cc6c898212a572e4a792ba882ad816481
uberflip-kumaran <kumaran.perinpanatan@uberflip.com>
2024-03-24 21:39:05 -0400

    comment oidc-provider

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ import userData from "./MOCK_DATA.json" assert { type: "json" };
 import graphql from "graphql";
 import { graphqlHTTP } from "express-graphql";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 
 const PORT = 5000;
@@ -92,34 +90,99 @@ app.get("/rest/getAllUsers", (req, res) => {
   res.send(userData);
 });
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // //Middlewares
 app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const configuration = {
-  clients: [
-    {
-      client_id: "oidcCLIENT",
-      client_secret: "Some_super_secret",
-      grant_types: ["authorization_code"],
-      redirect_uris: [
-        /* "http://localhost:8080/auth/login/callback",
-        "https://oidcdebugger.com/debug", */
-        "http://localhost:8055",
-      ],
-      response_types: ["code"],
+// const configuration = {
+//   key: [
+//     {
+//       alg: "RS256",
+//       e: "AQAB",
+//       key_ops: ["verify"],
+//       kty: "RSA",
+//       n: "0VJwEa4fiRDv4R6HMNEJZDR3_UAMGcrMhkR8aLhOLaOkV1sh84epY5xlpTmDiyCFf9bRDhSpnSB3QMOqzkBEZSlmd53P3hV-Wo-zRh48r1KzRdD6MDZU0oL-OyYgTP_VP1TZcT0oC5LG__p9Jex8SLpwzuHARpMuJyZ01wp5tUVkAbdGzf50-pQHoQNmXYoeojnG-alRMVH8gtuZ5oUn-h0GBSBspcAx5Jd_ifwX9uBkmYRjNFQQbslL69LZVcKvBwmPmeSxMI2TW0k8EJ-uR3Cw_5z1upG1vnKsKNpMuUsnwRduzncGaafz-MWDUIvVUYxMkRBjWRG0pPhDEb3E9w",
+//       use: "sig",
+//       kid: "c92834c9af14ade755dcefd90b5d7600",
+//     },
+//   ],
+//   clients: [
+//     {
+//       client_id: "oidcCLIENT",
+//       client_secret: "Some_super_secret",
+//       grant_types: ["authorization_code"],
+//       redirect_uris: [
+//         /* "http://localhost:8080/auth/login/callback",
+//         "https://oidcdebugger.com/debug", */
+//         "http://localhost:8055",
+//       ],
+//       features: {
+//         introspection: { enabled: true },
+//         revocation: { enabled: true },
+//       },
+//       formats: {
+//         AccessToken: "jwt",
+//       },
+//       claims: {
+//         openid: ["sub"],
+//         email: ["email", "email_verified"],
+//         profile: [
+//           "name",
+//           "family_name",
+//           "given_name",
+//           "profile",
+//           "picture",
+//           "updated_at",
+//         ],
+//       },
+//       async findAccount(ctx, id) {
+//         return {
+//           accountId: id,
+//           async claims(use, scope) {
+//             return {
+//               sub: id, // Usually a user id
+//               email: "user@example.com",
+//               email_verified: true,
+//               name: "John Doe",
+//               given_name: "John",
+//               family_name: "Doe",
+//               // Add more claims as per your application's requirements
+//             };
+//           },
+//         };
+//       },
+//       response_types: ["code"],
 
-      //other configurations if needed
-    },
-  ],
-  pkce: {
-    required: () => false,
-  },
-};
+//       //other configurations if needed
+//     },
+//   ],
+//   async findAccount(ctx, id) {
+//     return {
+//       accountId: id,
+//       async claims(use, scope) {
+//         return {
+//           sub: id, // Usually a user id
+//           email: "user@example.com",
+//           email_verified: true,
+//           name: "John Doe",
+//           given_name: "John",
+//           family_name: "Doe",
+//           preferred_username: "johndoe",
+//           // Add more claims as per your application's requirements
+//         };
+//       },
+//     };
+//   },
+//   pkce: {
+//     required: () => false,
+//   },
+// };
 
-const oidc = new Provider(`http://localhost:${PORT}`, configuration);
+// const oidc = new Provider(`http://localhost:${PORT}`, configuration);
 
-app.use("/oidc", oidc.callback());
+// app.use("/oidc", oidc.callback());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
